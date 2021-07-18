c1f70a962452f432ff47cfd4ee2a866a986375c1
rikki77 <87611778+rikki77@users.noreply.github.com>
2021-07-18 21:18:55 +0500

    add file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter one side of the right rectangle " << endl;
+
+    int a;
+    cin >> a;
+    cout << "Enter the second Number " << endl;
+    int b;
+    cin >> b;
+    cout << "Enter the Third Number " << endl;
+    int d;
+    cin >> d;
+    int J = a + b + d;
+
+    cout << "Answer is: " << J << "" << endl;
+
+
+    return 0;
+}
